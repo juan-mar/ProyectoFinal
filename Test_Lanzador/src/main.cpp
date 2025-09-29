fc41484be53214f31af784796faf3d49c27f61f8
juan-mar <juanmrodriguez@itba.edu.ar>
2025-09-29 00:41:55 -0300

    activacion de lanzador por teclado y serial

--- Test_Lanzador/src/main.cpp
+++ Test_Lanzador/src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+uint8_t TRIG_PIN = 5;                 // GPIO used to drive the pulse
+uint8_t PULSE_PIN = 14;
+unsigned long PULSE_DURATION_MS = 120; // Pulse length; adjust as needed
+
+void sendPulse() {
+  digitalWrite(PULSE_PIN, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
+  delay(PULSE_DURATION_MS);
+  digitalWrite(PULSE_PIN, LOW);
+  digitalWrite(TRIG_PIN, LOW);
+}
+
+//Para mosfet
+void sendPulseInv() {
+  digitalWrite(PULSE_PIN, LOW);
+  digitalWrite(TRIG_PIN, LOW);
+  delay(PULSE_DURATION_MS);
+  digitalWrite(PULSE_PIN, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(PULSE_PIN, OUTPUT);
+  digitalWrite(PULSE_PIN, LOW);
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  while (!Serial) {
+    ;
+  }
+
+  Serial.println(F("Listo: envie 'P' por serial para generar un pulso"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!Serial.available()) {
+    return;
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  char incoming = static_cast<char>(Serial.read());
+  if (incoming == 'P') {
+    sendPulse();
+    Serial.println(F("Pulso enviado"));
+  }
 }
