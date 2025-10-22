afb5616a9747b976fcaf4ba820a74d18a95bf3d3
Trinidad Greco <tgreco@itba.edu.ar>
2025-10-22 20:17:01 -0300

    destructor y funcion umbral

--- Test_ESP_Now/Test_Analisis_de_RSSI/CalibracionTAG/src/main.cpp
+++ Test_ESP_Now/Test_Analisis_de_RSSI/CalibracionTAG/src/main.cpp
@@ -12,22 +12,6 @@ void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
 
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_register_recv_cb(OnDataRecv);
-
-  //CB para el RSSI`
-  esp_wifi_set_promiscuous(true);
-  esp_wifi_set_promiscuous_rx_cb(&promiscuous_rx_cb);
 
 }
 
